9aeb5b1d2d6a2da2c3bb727a910e6485710d4348
Liquell <logoti228@gmail.com>
2022-07-27 00:52:03 +0300

    create site

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [],
+  theme: {
+    extend: {
+      
+    },
+    screens: {
+      'sm':'930px',
+      'mxl':'2100px'
+    }
+  },
+  plugins: [],
+}
